9d62f9684f1c2962a6faeaa518af689a8e947c7c
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2021-02-20 13:30:19 +0000

    added some arrows in

--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("forklift up")
+    basic.showArrow(ArrowNames.SouthEast)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showArrow(ArrowNames.West)
     radio.sendString("let")
 })
 input.onGesture(Gesture.ScreenDown, function () {
+    basic.showArrow(ArrowNames.SouthWest)
     radio.sendString("stop")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.NorthEast)
     radio.sendString("forklift down")
 })
 input.onGesture(Gesture.Shake, function () {
+    basic.showArrow(ArrowNames.NorthWest)
     radio.sendString("start")
 })
 input.onGesture(Gesture.TiltRight, function () {
