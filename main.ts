762c4f49a56d5cc7490b9a2e3834fad820e2b741
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2021-02-20 14:01:59 +0000

    added more commands

--- main.ts
+++ main.ts
@@ -2,9 +2,21 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("forklift up")
     basic.showArrow(ArrowNames.SouthEast)
 })
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.North)
+    radio.sendString("start")
+})
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showArrow(ArrowNames.West)
     radio.sendString("let")
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.No)
+    radio.sendString("stop")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Ghost)
+    radio.sendString("forklift stop")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
@@ -13,16 +25,12 @@ input.onButtonPressed(Button.B, function () {
     basic.showArrow(ArrowNames.NorthEast)
     radio.sendString("forklift down")
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showArrow(ArrowNames.NorthWest)
-    radio.sendString("start")
-})
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendString("right")
     basic.showArrow(ArrowNames.East)
 })
 input.onGesture(Gesture.LogoDown, function () {
-    basic.showArrow(ArrowNames.SouthWest)
-    radio.sendString("stop")
+    basic.showArrow(ArrowNames.South)
+    radio.sendString("back")
 })
 radio.setGroup(123)
