e66e50150a49f5bcd616b39c992a1bb7f434506f
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2021-02-20 13:09:56 +0000

    logo up and logo down

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("start")
 })
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.North)
+    radio.sendString("up")
+})
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showArrow(ArrowNames.West)
     radio.sendString("let")
@@ -14,5 +18,9 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendString("right")
     basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendString("down")
+    basic.showArrow(ArrowNames.South)
 })
 radio.setGroup(123)
